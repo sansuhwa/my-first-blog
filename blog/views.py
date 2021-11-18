d6e6a59f5e98fdc24ae0e9344c4bfd6dbb1cb1ac
JS <everi1007@gmail.com>
2021-11-18 13:49:56 +0900

    create views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
-# Create your views here.
+
+def post_list(requset):
+    qs = Post.objects.all()
+    qs = qs.filter(published_date__lte=timezone.now())
+    qs = qs.order_by('published_date')
+
+    return render(requset, 'blog/post_list.html', {
+        'post_list': qs,
+    })
