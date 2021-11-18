8ee7946169deceeb0236e9259245f71d5405b37d
JS <everi1007@gmail.com>
2021-11-18 12:41:49 +0900

    update blog_url

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from django.urls.resolvers import URLPattern
+from django.urls.resolvers import path
 from . import views
 
 
 URLPattern = [
     path('', views.post_list, name='psot_list'),
-    
+
 ]
